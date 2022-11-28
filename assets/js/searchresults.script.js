904ad486f4bb2d812e96f204798f182ca894cd77
Kenny Li <112835000+kcl20@users.noreply.github.com>
2022-11-27 21:10:50 -0500

    Added modal buttons.

--- assets/js/searchresults.script.js
+++ assets/js/searchresults.script.js
@@ -20,6 +20,7 @@ function getMovies() {
         })
         .then(function (data){
             console.log(data)
+            // pass data to loadMovies function
             loadMovies(data);
         })
 }
@@ -51,107 +52,181 @@ function loadMovies(data) {
     imdbIDs.push(data.Search[1].imdbID);
     imdbIDs.push(data.Search[2].imdbID);
     console.log(imdbIDs);
-    getIMDB(imdbIDs);
+    setupModal(imdbIDs);
+
 }
 
 
 
-function getIMDB(imdbIDs) {
+function setupModal(imdbIDs) {
 
     var movie0Button = document.createElement("button");
     movieEl0.append(movie0Button);
-    movie0Button.setAttribute("class", "modal-button");
+    movie0Button.setAttribute("class", "btn-open");
     movie0Button.setAttribute("id", "movieButton0");
-    movie0Button.textContent = "https://www.imdb.com/title/" + imdbIDs[0];
+    movie0Button.textContent = "More Info";
+    // movie0Button.textContent = "https://www.imdb.com/title/" + imdbIDs[0];
 
     var movie1Button = document.createElement("button");
     movieEl1.append(movie1Button);
-    movie1Button.setAttribute("class", "modal-button");
+    movie1Button.setAttribute("class", "btn-open");
     movie1Button.setAttribute("id", "movieButton1");
-    movie1Button.textContent = "https://www.imdb.com/title/" + imdbIDs[1];
+    movie1Button.textContent = "More Info";
+    // movie1Button.textContent = "https://www.imdb.com/title/" + imdbIDs[1];
 
     var movie2Button = document.createElement("button");
     movieEl2.append(movie2Button);
-    movie2Button.setAttribute("class", "modal-button");
+    movie2Button.setAttribute("class", "btn-open");
     movie2Button.setAttribute("id", "movieButton2");
-    movie2Button.textContent = "https://www.imdb.com/title/" + imdbIDs[2];
+    movie2Button.textContent = "More Info";
+    // movie2Button.textContent = "https://www.imdb.com/title/" + imdbIDs[2];
 
     
+    // getMovie1Info(imdbIDs);
+    // getMovie2Info(imdbIDs);
+    // getMovie3Info(imdbIDs);
 
-    // for (var i = 0; i < imdbIDs.length; i++) {
-    //     var requestIMDBUrl = "http://www.omdbapi.com/?i=" + imdbIDs[i] + "&apikey=dacedb99"
-        
-    //     fetch(requestIMDBUrl)
-    //         .then(function (response) {
-    //             return response.json();
-    //         })
-    //         .then(function (data) {
-    //             // console.log(data);
-    //         });                    
-    // }
-    getMovie1Info(imdbIDs);
-    getMovie2Info(imdbIDs);
-    getMovie3Info(imdbIDs)
-}
+    // MODAL
 
-function getMovie1Info (imdbIDs) {
-    var requestMovie1PlotUrl = "http://www.omdbapi.com/?i=" + imdbIDs[0] + "&full&apikey=dacedb99"
+    const modal = document.querySelector(".modal");
+    const overlay = document.querySelector(".overlay");
+    // const openModalBtn = document.querySelector(".btn-open");
+    var movie0Button = document.getElementById("movieButton0");
+    var movie1Button = document.getElementById("movieButton1");
+    var movie2Button = document.getElementById("movieButton2");
+    var rating = document.getElementById("rating");
+    var plot = document.getElementById("plot");
+    const closeModalBtn = document.querySelector(".btn-close");
+
+    const openModal0 = function () {
+        modal.classList.remove("hidden");
+        overlay.classList.remove("hidden");
+        var requestMovie0PlotUrl = "http://www.omdbapi.com/?i=" + imdbIDs[0] + "&full&apikey=dacedb99"
+        fetch(requestMovie0PlotUrl)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    var moviePlot = data.Plot
+                    var imdbRating = data.imdbRating
+                    rating.textContent = "IMDB Rating: " + imdbRating;
+                    plot.textContent = moviePlot;
+                });
+    };
+    movie0Button.addEventListener("click", openModal0);
+
+    const openModal1 = function () {
+        modal.classList.remove("hidden");
+        overlay.classList.remove("hidden");
+        var requestMovie1PlotUrl = "http://www.omdbapi.com/?i=" + imdbIDs[1] + "&full&apikey=dacedb99"
+        fetch(requestMovie1PlotUrl)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    var moviePlot = data.Plot
+                    var imdbRating = data.imdbRating
+                    rating.textContent = "IMDB Rating: " + imdbRating;
+                    plot.textContent = moviePlot;
+                });
+    }
+    movie1Button.addEventListener("click", openModal1);
+
+    const openModal2 = function () {
+        modal.classList.remove("hidden");
+        overlay.classList.remove("hidden");
+        var requestMovie2PlotUrl = "http://www.omdbapi.com/?i=" + imdbIDs[2] + "&full&apikey=dacedb99"
+        fetch(requestMovie2PlotUrl)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    var moviePlot = data.Plot
+                    var imdbRating = data.imdbRating
+                    rating.textContent = "IMDB Rating: " + imdbRating;
+                    plot.textContent = moviePlot;
+                });
+    }
+    movie2Button.addEventListener("click", openModal2);
+
+
+
+
+
+
+    const closeModal = function () {
+        modal.classList.add("hidden");
+        overlay.classList.add("hidden");
+    };
+
+    closeModalBtn.addEventListener("click", closeModal);
+
+
+
+
     
-    fetch(requestMovie1PlotUrl)
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
-                // console.log(data);
-                var moviePlot = data.Plot
-                var imdbRating = data.imdbRating
+    }
 
-            var movie1Info = [];
-            movie1Info.push(moviePlot)
-            movie1Info.push(imdbRating)
-
-            console.log(movie1Info)
-            });
-}
-
-function getMovie2Info (imdbIDs) {
-    var requestMovie2PlotUrl = "http://www.omdbapi.com/?i=" + imdbIDs[1] + "&full&apikey=dacedb99"
+// function getMovie1Info (imdbIDs) {
+//     var requestMovie1PlotUrl = "http://www.omdbapi.com/?i=" + imdbIDs[0] + "&full&apikey=dacedb99"
     
-    fetch(requestMovie2PlotUrl)
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
-                // console.log(data);
-                var moviePlot = data.Plot
-                var imdbRating = data.imdbRating
+//     fetch(requestMovie1PlotUrl)
+//             .then(function (response) {
+//                 return response.json();
+//             })
+//             .then(function (data) {
+//                 // console.log(data);
+//                 var moviePlot = data.Plot
+//                 var imdbRating = data.imdbRating
 
-            var movie2Info = [];
-            movie2Info.push(moviePlot)
-            movie2Info.push(imdbRating)
+//             var movie1Info = [];
+//             movie1Info.push(moviePlot)
+//             movie1Info.push(imdbRating)
 
-            console.log(movie2Info)
-            });
-}
+//             console.log(movie1Info)
+//             });
+// }
 
-function getMovie3Info (imdbIDs) {
-    var requestMovie3PlotUrl = "http://www.omdbapi.com/?i=" + imdbIDs[2] + "&full&apikey=dacedb99"
+// function getMovie2Info (imdbIDs) {
+//     var requestMovie2PlotUrl = "http://www.omdbapi.com/?i=" + imdbIDs[1] + "&full&apikey=dacedb99"
     
-    fetch(requestMovie3PlotUrl)
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
-                // console.log(data);
-                var moviePlot = data.Plot
-                var imdbRating = data.imdbRating
+//     fetch(requestMovie2PlotUrl)
+//             .then(function (response) {
+//                 return response.json();
+//             })
+//             .then(function (data) {
+//                 // console.log(data);
+//                 var moviePlot = data.Plot
+//                 var imdbRating = data.imdbRating
 
-            var movie3Info = [];
-            movie3Info.push(moviePlot)
-            movie3Info.push(imdbRating)
+//             var movie2Info = [];
+//             movie2Info.push(moviePlot)
+//             movie2Info.push(imdbRating)
 
-            console.log(movie3Info)
-            });
-}
+//             console.log(movie2Info)
+//             });
+// }
+
+// function getMovie3Info (imdbIDs) {
+//     var requestMovie3PlotUrl = "http://www.omdbapi.com/?i=" + imdbIDs[2] + "&full&apikey=dacedb99"
+    
+//     fetch(requestMovie3PlotUrl)
+//             .then(function (response) {
+//                 return response.json();
+//             })
+//             .then(function (data) {
+//                 // console.log(data);
+//                 var moviePlot = data.Plot
+//                 var imdbRating = data.imdbRating
+
+//             var movie3Info = [];
+//             movie3Info.push(moviePlot)
+//             movie3Info.push(imdbRating)
+
+//             console.log(movie3Info)
+//             });
+// }
 // initialize function to lookup movies via API
 getMovies();
+
+
